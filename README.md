c28f4f57ff39f64bd319ea766520a4df22f5acae
name <name@utility1.moreinternal.com>
2024-11-11 12:51:31 -0500

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,115 @@
 # jenkins-docker-cloud-tls
-Example automation for jenkins controller using JCASC to talk to Docker rootless over api with jobs
+
+# Description:
+Example of running jenkins controller from a docker container running rootless, using docker API over TLS, creating a docker cloud in jenkins, and usng jenkins CASC (configuration as code).   There's lots of articles, gists, github repos etc that cover one area of running jenkins or another. The goal here was to put together an automated deployment end to end.  Therefore any changes are source controlled and trackable.
+
+This project uses jenkins casc (configuration as code) to configure the controller. Jenkins plugins, and credentials for connecting to the docker api as well as certificate and keystores are injected during the container build. The casc configuration includes set up of a docker cloud on the jenkins controller, and push a test job that will exercise running agents as containers.
+
+The script will check requirements, configure, build, and run the container, generate all certs and embed into the containers as needed.  The script will also attempt to clean up resources, so be mindful of running it if you have stopped containers or unused volumes you wish to preserve.  (It will prompt before deleting dangling volumes or containers)
+
+The script is opinionated, and assumes the following pre-requisite setup:
+Docker running rootless as a local user, as well as docker to be accessible via API using TLS with self signed certs. Access to the CA cert, key and API cert are needed for building the container.  
+
+If all goes to plan (and you imported the CA cert you created into a browser), you should be able to access your jenkins controller via browser at https://[fqdn of your host]:8443 and you can run the sample job created.
+
+# Using the script and configuration template
+Before running the setup.sh, there are a few manual edits to be made as they pertain to your environment.  Future variations of this project will attempt to do that automatically.  At the very least you should confirm these settings before running the script. Sections further below is a table outlining the files and their usage. The following codeblocks will need to be edited manually:
+
+- casc.yaml ( 2 sections need editing)
+1. "replace from script" should be replaced with the certificate contents for Docker API access
+```    
+    - credentials:
+      - x509ClientCert:
+          clientCertificate: |-
+            -----BEGIN CERTIFICATE-----
+            replace from script
+            -----END CERTIFICATE-----
+          clientKeySecret: |-
+            -----BEGIN PRIVATE KEY-----
+            replace from script
+            -----END PRIVATE KEY-----
+          id: "docker-api"
+          scope: GLOBAL
+          serverCaCertificate: |-
+            -----BEGIN CERTIFICATE-----
+            replace from script
+            -----END CERTIFICATE-----
+```
+2. The jenkins admin user password should be changed, note that you will also need to update it in setup.sh
+```
+ users:
+      - id: "admin"
+        name: "admin"
+        password: "jenkins"
+```
+- setup.sh
+(Update the auth portion of the curl command to coincide with what you used above in casc.yaml)
+```
+-auth admin:jenkins
+```
+
+# Basic Requirements:
+- Docker Compose V2
+- Docker Rootless configured and using TLS for API connections to the socket
+- Ubuntu 24 (should work on other distros as well, albeit minor modifications may be required)
+- python 3 with pip (required by docker compose)
+- Java Open JDK installed (needed for keytool, and jenkins-cli). Other java installations likely ok.
+- OpenSSL installed (needed for certificate and CA operations)
+- Curl installed
+- Wget installed
+- Firefox or Chrome browser and understanding of how to import a CA into them. (examples in the links section)
+- fqdn properly set for the host, and host resolvable by ip other than localhost
+
+# Warnings and Recommendations:
+The secrets used for the jenkins admin user and keystore should also be changed prior to production use.
+While the .gitignore file is in place, to prevent accidental storing of secrets and credentials, caution should be applied when modifying any templates to contain secrets so as to prevent accidental upload to git such as the yaml files or env files, which are not protected by .gitignore.
+
+Commercially purchased and trusted certs are better. This example uses self signed for demonstration purposes only, in a production environment, it's recommended to use commercially purchased or domain generated certs that align with your environment.
+
+# File descriptions and usage:
+| filename | description |
+| ---------| ----------- |
+| casc.yaml | jenkins casc configuration |       
+| DockerfileGetcacerts | dockerfile used to get cacerts from exploded filesystem |
+| jenkins-controller-docker-compose.env | env vars passed to docker compose |
+| jenkins-controller-docker-compose.yaml | docker compose file for controller, volumes, network |
+| JenkinsDockerfile | dockerfile used to build customized controller |
+| plugins.txt | text listing of plugins and version used to preconfigure jenkins controller |
+| setup.sh | bash script to perform basic checks, create CAs, certs, and keystores, and stand up containers |
+| TestAgent.xml | sample job uploaded to exercise agent container |
+
+# Script General Workflow:
+While the shell script is commented, below is an overview of what it does.  
+
+1. Basic check that binary pre-reqs at least exist (not an exhaustive check that they run, or installed properly)
+
+2. Rudimentary check that docker is running rootless, and accessible via API using tls authenticaiton
+
+3. Pull a copy of the cacerts from the latest controller image, and inject our Docker API tls certs into the cacerts file so jenkins can communicate with docker via API.  This requires exploding the image to a filesystem, copying out the cacerts file, and deleting the intermediate exploded filesystem.
+
+4. Generate a CA, and self signed certs for jenkins controller to be able to be accessed by the browser using tls/https. It injects these into a keystore for consumption by Jenkins. The CA for jenkins will need to be manually imported into your browser to prevent untrusted certificate errors while accessing the application in the browser.
+
+5.  Build the container from Dockerfile which will use the certs created in previous steps, install plugins necessary from a list, mount volumes, and configure the controller using Jenkins CASC to create an initial user, some basic creds, and minimal server configuration, and avoid using the install wizard. 
+The minimal jenkins configuration includes creating a credential to talk to docker via API, as well as setting up the docker cloud, and an agent configuration run from a separate container on the local docker cloud. Docker compose will then be called to stand up the container, volume, and network. Lastly there is a basic attempt at verifying the container is running, and that the jenkins application is up and responding.
+
+6. We'll pull the latest jenkins-cli jar from the running application, and use it to upload a sample job. To run the job, log into the application via browser, which, when run, will dynamically spin a container running in docker as an agent and run a simple echo command, run in the agent container. At conclusion of the job, the agent container is stopped.
+
+# Future Improvements, Links, and TLDR;
+
+The purpose of this example was to create an starting foundation for deployment automation of jenkins casc, containerization, and using tls/https.  While every attempt was made to be aligned with security best practices, there is always room for improvement. While this should work out of the box with minimal changes, it's not expected that one would use it without customization. The expectation is that some modification would be made to secure, and operationalize this to meet your work environment, and this is merely a quickstart. Future versions will be more opinionated and likely force this. 
+
+Planned Future improvements:
+- More parameterization of secrets and inputs reducing the chance of storing secrets in source
+- Parameterizing to support different Jenkins images besides lts-jdk17
+- More thorough checks and balances, error handling, retrym ability to skip checks, and cleanup steps
+- Automate CA cert import into Chrome/Firefox
+- Look at using let's encrypt or other certificate solutions
+- Add lifecycle to the script: initial deploy, backup (before delete), re-deploy, update etc
+- Expand to other distros besides ubuntu 23,24
+
+Links:
+1. https://hub.docker.com/r/jenkins/jenkins  ( Jenkins Controller Image used in this example )
+2. https://hub.docker.com/r/jenkins/agent/   ( Jenkins Agent Image used in this example )
+3. https://www.jenkins.io/doc/book/managing/casc/  ( Jenkins casc reference and documentation )
+4. https://wiki.mozilla.org/CA/AddRootToFirefox   (Importing CA cert into firefox)
+5. https://support.google.com/chrome/a/answer/3505249?hl=en  (Importing CA cert into chrome)
---
+++ setup.sh
@@ -0,0 +1,283 @@
+#!/bin/bash
+
+check_binary_available () {
+    binarylist=(docker openssl keytool wget java curl)
+    for i in "${binarylist[@]}"; do
+        # Check if the binary exists
+        if command -v "$i" >/dev/null 2>&1; then
+            echo "$i is installed."
+        else
+            echo "$i is not installed, exiting."
+            exit -1
+        fi    
+    done
+}
+
+check_docker_rootless() {
+    # check output of docker context
+    DockerContextOutput=$(docker context show)
+    if [[ "$DockerContextOutput" != "rootless" ]]; then
+        echo "Docker Rootless not in use, exiting.";
+        exit -1
+    fi
+    # check dockerd process owner
+    ps -u $username | grep dockerd >/dev/null 2>&1
+    if [ "$?" -ne 0 ]; then
+        echo "Docker doesn't appear to be running rootless as $username, exiting."
+        exit -1
+    fi
+    # check containerd process owner
+    ps -u $username | grep containerd >/dev/null 2>&1
+    if [ "$?" -ne 0 ]; then
+        echo "Containerd doesn't appear to be running as $username, exiting."
+        exit -1
+    fi
+    echo "Docker rootless and containerd appears functional."
+}
+
+check_docker_api () {
+    # verify docker api is functional and tls connection ok
+    # opinionated location of docker API certs follows
+    path2certs="/home/$username/.docker/certs/"
+    filelist=(ca-cert.pem server-cert.pem server-key.pem)
+    # check all 3 files are there, exit otherwise
+    for i in "${filelist[@]}"; do
+        if [ ! -f $path2certs/$i ]; then
+            echo "$path2certs/$i is missing, can't check docker api over tls"
+            echo "Need Docker API CA, Cert and Key to communicate over TLS"
+            echo "exiting."
+            exit -1
+        fi    
+    done
+    # run docker info, specify context and paths to certs for tls, check return code,
+    # nonzero means something wrong
+    DockerInfoOutput=$(docker --context rootless --tlsverify --tlscacert /home/$username/.docker/certs/ca-cert.pem --tlscert /home/name/.docker/certs/server-cert.pem --tlskey /home/name/.docker/certs/server-key.pem info 2>/dev/null)
+    DockerInfoReturnCode=$?
+    if [ "$DockerInfoReturnCode" -ne 0 ]; then
+        echo "Docker Info via API failed, exiting."
+        echo "$DockerInfoOutput"
+        exit -1
+    else
+        echo "Docker API appears functional."
+    fi
+    #verify context is rootless from API
+    if [[ $DockerInfoOutput == *"Context:    rootless"* ]]; then
+        echo "API response indicates rootless mode in use."
+    else
+        echo "API response doesn't indicate rootless context in use, exiting."
+        exit -1
+    fi
+    #verify connection to running host matches hostname
+    if [[ $DockerInfoOutput == *"Name: $hostname"* ]]; then
+        echo "API response indicates connection to $hostname is good."
+    else
+        echo "API response indicates connection to $hostname is bad, exiting."
+        exit -1
+    fi
+}
+
+clean_sandbox () {
+    echo "cleaning intermediate files if they exist."
+    filelist=(cacerts docker_api_root_ca.pem ca-cert.srl ca-key.pem server-req.pem server-key.pem server-ext.cnf server-cert.pem jenkins_keystore.jks jenkins.p12 jenkins-cli.jar)
+    for i in "${filelist[@]}"; do
+        if [ -f $i ]; then
+            rm -v $i
+        fi    
+    done
+    #make sure container, volumes don't exist already (take stack down, worst case doesn't exist yet, otherwise old gets cleaned up on the fly)
+    CONTAINER_NAME=jenkins-controller-1 JENKINS_HOME=jenkins-home-1 docker compose -f jenkins-controller-docker-compose.yaml down -v
+    echo "Removing dangling containers if they exist."
+    docker image prune --filter "dangling=true"
+    #todo make sure docker volume ls isn't empty before running this
+    echo "Remove unused volumes if they exist."
+    volumecount=$(docker volume ls --format "{{.Name}}" | wc -l)
+    if [ "$volumecount" -ne 0 ]; then
+        docker volume rm `docker volume ls -q dangling=true`
+    fi
+}
+
+check_certificate () {
+    inputcert=$1
+    # check valid for at least 2 weeks
+    dockcacheckoutput=$(openssl x509 -in $inputcert -text -noout -checkend "1209600")
+    if [[ $dockcacheckoutput == *"Certificate will not expire"* ]]; then
+        echo "$inputcert is good for at least 2 weeks."
+    else
+        echo "$inputcert will expire in less than 2 weeks, exiting."
+        exit -1
+    fi
+    # opinionated check certificate CommonName matches hostname
+    if [[ $dockcacheckoutput == *"CN = $hostname"* ]]; then
+        echo "$inputcert CN matches hostname."
+    else
+        echo "$inputcert CN doesn't match hostname, exiting."
+        exit -1
+    fi
+
+}
+
+pull_latest_image () {
+    # pull latest jenkins controller and agent, or update what's already in the local registry
+    ImagePullOutput=$(docker pull $1)
+    ImagePullReturnCode=$?
+    ImagePullUpToDateCheck="Status: Image is up to date for $1"
+    if [ "$ImagePullReturnCode" -ne 0 ]; then
+        echo "Image $1 Update Failed."
+        echo "$ImagePullOutput"
+        exit -1
+    fi
+    if echo "$ImagePullOutput" | grep -q "$ImagePullUpToDateCheck"; then
+        echo "Image $1 Is already up to date.";
+    else
+        echo "Image $1 Was updated to the latest.";
+    fi
+}
+
+generate_controller_cacerts () {
+    # get ca cert from docker api 
+    openssl s_client -showcerts -connect $hostname:2376 </dev/null 2>/dev/null | openssl x509 -outform PEM > docker_api_root_ca.pem
+    check_certificate docker_api_root_ca.pem
+
+    # programatically get cacerts file from existing jenkins controller container
+    # delete exploded filesystem when done
+    echo 'FROM jenkins/jenkins:lts-jdk17' > DockerfileGetcacerts
+    docker build -f DockerfileGetcacerts -o jenkinsrootfs .
+    cp -v jenkinsrootfs/opt/java/openjdk/lib/security/cacerts cacerts
+    rm -rf jenkinsrootfs
+
+    #import ca cert pem into cacerts keystore from controller container (allows jenkins to talk to Docker API over tls)
+    keytoolimportoutput=$(keytool -import -noprompt -trustcacerts -storepass changeit -file docker_api_root_ca.pem -alias $hostname-DockerCA -keystore cacerts)
+    if [ "$?" -ne 0 ]; then
+        echo "keytool import of ca cert pem into cacerts failed, exiting."
+        echo "$keytoolimportoutput"
+        exit -1
+    else
+        echo "keytool import of ca cert pem into cacerts appears successful."
+    fi
+    # check cacerts contains docker api CA using keytool
+    controllercacertscheck=$(keytool -list -keystore cacerts -alias $hostname-DockerCA -storepass changeit)
+    if [[ $controllercacertscheck =~ $hostname-DockerCA && $controllercacertscheck =~ "trustedCertEntry" ]] then
+        echo "keytool lists Docker API CA alias in keystore cacerts as trusted entry."
+    else
+        echo "keytool isnt showing Docker API  CA cert imported correctly"
+        exit -1
+    fi
+}
+
+generate_jenkins_app_certs_and_keystore () {
+    #generate certs for tls encryption of jenkins controller
+    echo "generate CA"
+    openssl req -x509 -newkey rsa:4096 -days 360 -nodes -keyout ca-key.pem -out ca-cert.pem -subj "/C=US/ST=MA/L=Boston/O=Self/OU=jenkins/CN=$hostname-CA/emailAddress=" >/dev/null 2>&1
+    echo "generate jenkins server priv key and csr"
+    openssl req -newkey rsa:4096 -keyout server-key.pem -nodes -out server-req.pem -subj "/C=US/ST=MA/L=Boston/O=Self/OU=jenkins/CN=$hostname-Server/emailAddress=" >/dev/null 2>&1
+    echo "generate alt names file for cert"
+    echo "subjectAltName=DNS:$hostnameshort,DNS:$hostname,IP:$ip" > server-ext.cnf
+    echo "generate jenkins server cert"
+    openssl x509 -req -in server-req.pem -days 360 -CA ca-cert.pem -CAkey ca-key.pem -CAcreateserial -out server-cert.pem -extfile server-ext.cnf >/dev/null 2>&1
+    # check cert won't expire and CommonName incudes hostname
+    check_certificate server-cert.pem
+    #verify generated server cert against CA
+    servercertcheck=$(openssl verify -CAfile ca-cert.pem server-cert.pem)
+    if [[ $servercertcheck =~ "server-cert.pem: OK" ]] then
+        echo "Generated CA and cert passed verification."
+    else
+        echo "Generated CA and cert did not pass verification, exiting."
+        exit -1
+    fi
+    echo "generate jenkins keystore to hold self signed cert"
+    keytool -genkey -dname "cn=jenkins, ou=$hostnameshort, o=$domainname, c=US" -keyalg RSA -alias jenkinselfsigned -keystore jenkins_keystore.jks -storepass changeit -keysize 4096 -validity 365
+    echo "create pkcs12 file of server cert and key"
+    openssl pkcs12 -export -in server-cert.pem -inkey server-key.pem -out jenkins.p12 -password pass:changeit
+    echo "import pkcs12 file to keystore"
+    keytool -importkeystore -noprompt -srckeystore jenkins.p12 -srcstoretype PKCS12 -destkeystore jenkins_keystore.jks -deststoretype JKS -deststorepass changeit -srcstorepass changeit
+    echo "import server ca to keystore"
+    keytool -importcert -noprompt -keystore jenkins_keystore.jks -trustcacerts -alias $hostname-JenkinsCA -file ca-cert.pem -deststorepass changeit
+}
+
+update_jenkins_casc () {
+    # todo: update URL and other items that rely on hostname
+    # todo: programatically insert docker api credential from local files 
+    # so that jenkins can talk to docker api
+}
+
+build_container_and_run_stack () {
+    # build container and stand up application
+    # todo: stack name from date/time rather than static name
+    echo "Running docker compose and standing up app stack"
+    CONTAINER_NAME=jenkins-controller-1 JENKINS_HOME=jenkins-home-1 docker compose -f jenkins-controller-docker-compose.yaml up --build -d
+    echo "Verify container running post compose"
+    container_running_check="docker container inspect -f '{{.State.Status}}' jenkins-controller-1"
+    container_running=1
+    while [ $container_running -eq 1 ]
+    do 
+        container_check_output=$($container_running_check)
+        if [[ "$container_check_output" =~ "running" ]]; then
+            echo "container now running"
+            container_running=0
+        else
+            for i in {1..3}; do 
+                echo -n "."
+                sleep 1 
+            done
+        fi
+    done
+    
+    # check jenkins app is up and responding before job upload
+    echo "Check jenkins app responding via curl"
+    app_running_check="curl -Isk https://$hostname:8443/jnlpJars/jenkins-cli.jar | head -1"
+    app_running=1
+    while [ $app_running -eq 1 ]
+    do 
+        app_check_output=$($app_running_check)
+        if [[ "$app_check_output" =~ "HTTP/1.1 200 OK" ]]; then
+            echo "jenkins app now running"
+            app_running=0
+        else
+            for i in {1..3}; do 
+                echo -n "."
+                sleep 1 
+            done
+        fi
+    done
+}
+# end function defs
+
+# get some basic host and user details to be passed on to functions as inputs
+hostname=$(hostname)
+hostnameshort=$(hostname -s)
+domainname=$(hostname -d)
+ip=$(hostname -I)
+username=$(whoami)
+uid=$(id -u)
+
+# check binary pre-req's
+check_binary_available
+
+# check runtime components
+check_docker_rootless
+check_docker_api
+
+# get latest container images
+pull_latest_image "jenkins/jenkins:lts-jdk17"
+pull_latest_image "jenkins/agent:jdk17"
+
+# make sure clean sandbox before cert generations, and keystore operations
+clean_sandbox
+
+# get clean cacerts, inject Docker API CA cert so jenks can talk to docker over tls
+# we'll add the updated cacerts to the container when its built through copy layer
+generate_controller_cacerts
+
+# create ca, and server certs to talk with local browser and other utils
+# we'll add these to the cotnainer when its built through copy layer
+generate_jenkins_app_certs_and_keystore
+
+# run docker compose and check container up and test https connection 
+build_container_and_run_stack
+
+# get latest cli jar and import test job
+echo "retrieving latest jenkins cli jar from app"
+wget -q https://$hostname:8443/jnlpJars/jenkins-cli.jar --no-check-certificate
+
+echo "uploading a container agent test job"
+java -Djavax.net.ssl.trustStore=jenkins_keystore.jks -Djavax.net.ssl.trustStorePassword=changeit -jar jenkins-cli.jar -auth admin:jenkins -s https://$hostname:8443/ create-job "Test Agent" < TestAgent.xml
